a038b44b48c89ee54c7d893da5c0bad28028dcd8
joshuasandovalb11 <erickjsandoval2000@gmail.com>
2026-05-25 13:33:34 -0700

    Actualizacion de contactos por vendedor

--- api/verify-user.js
+++ api/verify-user.js
@@ -1,30 +1,29 @@
-// --- LISTA DE USUARIOS AUTORIZADOS ---
+// LISTA DE USUARIOS AUTORIZADOS
 const authorizedUsers = [
   { phone: "6644114499", name: "BARBOSA HIGUERA EDGAR" },
-  { phone: "6621900175", name: "CABANILLAS GAXIOLA CARLOS" },
   { phone: "6642282840", name: "CERVANTES GONZALEZ LETICIA" },
   { phone: "6642229474", name: "DIAZ MILLAN MARCELINA" },
   { phone: "6644049974", name: "FLORES VELAZQUEZ DENISSE" },
   { phone: "6644044293", name: "GARCIA REYES LAURA" },
-  { phone: "6644114456", name: "GARCIA VEGA JULIAN" },
   { phone: "6644114629", name: "GONZALEZ LARA ANA LILIA" },
   { phone: "6644046318", name: "HERRERA LOPEZ CARLA" },
   { phone: "6643866552", name: "MARTINEZ MADERA GABRIELA" },
-  { phone: "6643854677", name: "MEDINA JARAMILLO MARLIN" },
+  { phone: "6643084034", name: "MATEO DE LOS SANTOS KAREN" },
+  { phone: "6644114555", name: "MEDINA JARAMILLO MARLIN" },
   { phone: "6644044073", name: "MEDRANO ELIZALDE JESUS" },
-  { phone: "6643084034", name: "MONTES PINEDA MARIO ENRIQUE" },
-  { phone: "6644114555", name: "OLIVAS BATRES RODRIGO" },
-  { phone: "6642252357", name: "PENA GONZALES JESUS" },
+  { phone: "6643638127", name: "MORENO MUÑOZ MARITZA" },
+  { phone: "6642252357", name: "OLIVAS BATRES RODRIGO" },
+  { phone: "6644114456", name: "PENA GONZALES JESUS" },
   { phone: "6624254723", name: "PEREZ HERNANDEZ JOEL" },
   { phone: "6642223859", name: "RENTERIA RODRIGUEZ YOLANDA" },
   { phone: "6643979685", name: "REYES NEVARES ANA MARIA" },
   { phone: "6632032986", name: "RODRIGUEZ IBARRA ANTONIA" },
   { phone: "6642225410", name: "RODRIGUEZ PRECIADO VIRIDIANA" },
+  { phone: "6621900175", name: "RUIZ NAVARRO JAVIER" },
   { phone: "6644084875", name: "TAPIA LEDEZMA ANDRES" },
   { phone: "6121774238", name: "MARES MORALES JOSE RAFAEL" },
   { phone: "6622974290", name: "Blanca Ramirez" },
-  { phone: "6621016488", name: "Javier Ruiz Navarro" },
-  { phone: "6648171212", name: "PRUEBA" },
+  { phone: "6647099372", name: "PRUEBA" },
 ];
 
 export default async function handler(req, res) {
@@ -41,7 +40,7 @@ export default async function handler(req, res) {
   }
 
   const user = authorizedUsers.find(
-    (u) => u.phone === user_phone_number.trim()
+    (u) => u.phone === user_phone_number.trim(),
   );
 
   if (user) {
